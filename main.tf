30b5f36ee9e57cf6976a24faedcb2636593d634a
MayankYerragondu <myerragondu@gmail.com>
2025-09-16 10:24:32 -0400

    update main terraform
    add more modules

--- main.tf
+++ main.tf
@@ -6,6 +6,35 @@ module "s3" {
   source = "./s3"
 }
 
+module "iam" {
+  source = "./iam"
+}
+
+module "glue_jobs" {
+  source         = "./glue_jobs"
+  bucket_name    = module.s3.bucket_name
+  glue_role_arn  = module.iam.glue_role_arn
+}
+
+module "ecr_sagemaker" {
+  source = "./ecr"
+  name   = "sagemaker-custom-image"
+  tags = {
+    Project = "iot-glue-training"
+  }
+}
+
+module "glue_workflow" {
+  source                  = "./glue_workflow"
+  glue_role_arn           = module.iam.glue_role_arn
+  glue_script_path        = module.s3.extract_script_path
+  cleaning_script_path    = module.s3.cleaning_script_path
+  bucket_name             = module.s3.bucket_name
+  extract_job_name        = module.glue_jobs.extract_job_name
+  cleaning_job_name       = module.glue_jobs.cleaning_job_name
+}
+
+
 
 resource "aws_sfn_state_machine" "sagemaker_training" {
   name     = "sagemaker-training-job"
