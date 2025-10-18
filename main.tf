f28410b117aaed4fb0e13764b72d93511597164d
MayankYerragondu <myerragondu@gmail.com>
2025-10-18 18:59:47 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,21 +4,27 @@ provider "aws" {
 
 module "s3" {
   source = "./s3"
+  providers = { aws = aws }
 }
 
 module "iam" {
   source = "./iam"
+  providers = { aws = aws }
 }
 
 module "glue_jobs" {
   source         = "./glue_jobs"
   bucket_name    = module.s3.bucket_name
   glue_role_arn  = module.iam.glue_role_arn
+  providers = { aws = aws }
+
 }
+
+
 
 module "ecr_sagemaker" {
   source = "./ecr"
-  name   = "sagemaker-custom-image"
+  name   = "sagemaker-custom-image-1"
   tags = {
     Project = "iot-glue-training"
   }
@@ -32,12 +38,13 @@ module "glue_workflow" {
   bucket_name             = module.s3.bucket_name
   extract_job_name        = module.glue_jobs.extract_job_name
   cleaning_job_name       = module.glue_jobs.cleaning_job_name
+  providers = { aws = aws }
 }
 
 
 
 resource "aws_sfn_state_machine" "sagemaker_training" {
-  name     = "sagemaker-training-job"
+  name     = "sagemaker-training-job-1"
   role_arn = module.iam.stepfunction_role_arn
 
   depends_on = [
