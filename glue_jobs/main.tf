59c0d9e31425f8e4a0e356376b359eff4b906d9b
MayankYerragondu <myerragondu@gmail.com>
2025-10-18 19:00:20 -0400

    Update main.tf

--- glue_jobs/main.tf
+++ glue_jobs/main.tf
@@ -1,8 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 variable "bucket_name" {}
 variable "glue_role_arn" {}
 
 resource "aws_glue_job" "extract" {
-  name     = "iot-sensor-extract"
+  name     = "iot-sensor-extract-1"
   role_arn = var.glue_role_arn
 
   command {
@@ -15,6 +24,7 @@ resource "aws_glue_job" "extract" {
     "--TempDir"      = "s3://${var.bucket_name}/temp/"
     "--job-language" = "python"
     "--output_bucket"  = "${var.bucket_name}/output/"
+    "--bucket_name"  = var.bucket_name
   }
 
   glue_version      = "4.0"
@@ -23,7 +33,7 @@ resource "aws_glue_job" "extract" {
 }
 
 resource "aws_glue_job" "cleaning" {
-  name     = "iot-data-cleaning"
+  name     = "iot-data-cleaning-1"
   role_arn = var.glue_role_arn
 
   command {
@@ -36,9 +46,18 @@ resource "aws_glue_job" "cleaning" {
     "--TempDir"      = "s3://${var.bucket_name}/temp/"
     "--job-language" = "python"
     "--output_bucket"  = "${var.bucket_name}/output/"
+    "--bucket_name"  = var.bucket_name
   }
 
   glue_version      = "4.0"
   number_of_workers = 2
   worker_type       = "G.1X"
+}
+
+output "extract_job_name" {
+  value = aws_glue_job.extract.name
+}
+
+output "cleaning_job_name" {
+  value = aws_glue_job.cleaning.name
 }
